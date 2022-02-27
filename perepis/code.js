193c3ab3efe237751d48d6e9ccb70bc414eea311
Serla0 <96717689+Serla0@users.noreply.github.com>
2022-02-27 22:16:57 +0800

    Update code.js

--- perepis/code.js
+++ perepis/code.js
@@ -1,12 +1,11 @@
 let main = document.querySelector(".main");
 let input_1 = document.querySelector(".input_1");
 let but_1 = document.querySelector(".but_1");
-let lett;
-function glava() {
+// function glava() {
     // отследить Enter
     localStorage.setItem("ret", "<p>" + document.querySelector(".input_1").value + "</p>");
     main.innerHTML += localStorage.getItem("ret");
     localStorage.setItem("rey", main.innerHTML);
-}
+// }
 main.innerHTML = localStorage.getItem("rey");
 // localStorage.clear();
